7dbf220be33f3a9e00077252b5c4438bc397357f
DevAnas17 <anasdeveloper17@gmail.com>
2022-12-05 21:07:29 +0530

    add contact form

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,7 +5,7 @@ import { BsFillMoonStarsFill } from "react-icons/bs";
 import {
     AiFillTwitterCircle,
     AiFillLinkedin,
-    AiFillYoutube,
+    AiFillGithub
   } from "react-icons/ai";
 import Image from "next/image"
 
@@ -26,12 +26,12 @@ const Header = ({darkMode, setDarkMode}) => {
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="#"
                 >
-                  Resume
-                </a>
+                  Resume 
+                </a> 
               </li>
             </ul>
-          </nav>
-          <div className="text-center p-10 py-10">
+          </nav> 
+          <div className="text-center p-8 py-8">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Anas Abbasi
             </h2>
@@ -43,11 +43,19 @@ const Header = ({darkMode, setDarkMode}) => {
               needs. Join me down below and let's get cracking!
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
+              <a href="https://github.com/Devanas17" target="_blank">
               <AiFillTwitterCircle />
+              </a>
+
+              <a href="https://www.linkedin.com/in/developeranas/" target="_blank">
               <AiFillLinkedin />
-              <AiFillYoutube />
+              </a>
+              
+              <a href="https://github.com/Devanas17" target="_blank">
+              <AiFillGithub />
+              </a>
             </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-40 h-40 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={anas} layout="fill" objectFit="cover" />
             </div>
           </div>
--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
+require("dotenv").config();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    USER_ID: process.env.USER_ID,
+    SERVICE_ID: process.env.SERVICE_ID,
+    TEMPLATE_ID: process.env.TEMPLATE_ID,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
